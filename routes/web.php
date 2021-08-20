5036780257e452dee2533eeedbfd20039a92f8ef
Salim66 <salimhasanriad8@gmail.com>
2021-08-20 12:13:05 +0600

    Updating Data

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,22 @@ Route::get('/read', function(){
     }
 
 });
+
+Route::get('/update', function(){
+
+    $user = User::findOrFail(1);
+
+    if($user->has('roles')){
+
+        foreach($user->roles as $role){
+
+            if($role->name == 'administrator'){
+                $role->name = 'subscriber';
+                $role->save();
+            }
+
+        }
+
+    }
+
+});
